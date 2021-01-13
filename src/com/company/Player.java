fd2b6a22ed5a73f497d42881d0cfa66c780c2972
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-01-13 19:11:12 +0100

    Player: Changed setMoneyAmount to addMoneyAmount with a parameter. Added getter and setter for the arraylist and methods to remove and add items from the arraylist.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,5 @@
 package com.company;
+
 import java.util.ArrayList;
 
 public class Player {
@@ -21,8 +22,26 @@ public class Player {
     public int getMoneyAmount(){
         return this.moneyAmount;
     }
-    public void setMoneyAmount(){
-        this.moneyAmount += 10;
 
+    public void addMoneyAmount(int moneyChange){
+        this.moneyAmount += moneyChange;
+        // TODO Must be changed to add the amount of money the animal costs when sold
+
+    }
+
+    public ArrayList<Animal>getAnimals(){
+        return this.animals;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void addAnimal(Animal animal){
+        this.animals.add(animal);
+    }
+
+    public void removeAnimal(Animal animal){
+        this.animals.remove(animal);
     }
 }
