e5fa0960b1a4975d94a0a2d9605c4eb66384abef
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-01-13 19:12:12 +0100

    Store: Started working on sellAnimal and buyAnimal methods.

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -1,10 +1,28 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Store {
 
     public void buyAnimal(){
+        Scanner scan = new Scanner(System.in);
+        Animal boughtAnimal = null;
+
+        System.out.println("""
+                Which animal do you want to buy?\s
+                |1| Nykur\s
+                |2| Gloson\s
+                |3| Kraken\s
+                |4| Linnr\s
+                |5| Tilberi""");
+
+        try{
+            int input = Integer.parseInt(scan.nextLine());
+        }
+        catch(NumberFormatException e){
+            System.out.println("You have to enter a number belonging to an animal in the above list");
+        }
+
 
     }
 
@@ -12,7 +30,32 @@ public class Store {
 
     }
 
-    public void sellAnimal(){
+    public int sellAnimal(){
+        int health = 0; //TODO make health be health of animal sent to method
+        int price = 0;
+        if(getClass().getSimpleName().equals("Nykur")){
+            price = 30;
+        }
+        else if(getClass().getSimpleName().equals("Gloson")){
+            price = 40;
+        }
+        else if(getClass().getSimpleName().equals("Kraken")){
+            price = 10;
+        }
+        else if(getClass().getSimpleName().equals("Linnr")){
+            price = 20;
+        }
+        else if(getClass().getSimpleName().equals("Tilberi")){
+            price = 50;
+        }
+        price *= health;
+        return price;
+
+        // TODO If the class of the animal is given value
+        //  Return price
+    }
+
+    public void sellAllAnimals(){
 
     }
 
