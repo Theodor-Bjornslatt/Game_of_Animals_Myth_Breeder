ec9b0d440ef765387acb280483c1bbefa8c1f3ec
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-01-14 19:06:25 +0100

    Changed int moneyAmount to goldAmount and changed setter and getter accordingly
    Created method specifically for adding gold

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -8,7 +8,7 @@ public class Player {
     ArrayList<Food>food = new ArrayList<>();
 
     private String name;
-    private int moneyAmount = 10;
+    private int goldAmount = 0;
 
 
     public Player(String name){
@@ -19,17 +19,25 @@ public class Player {
         return this.name;
     }
 
-    public int getMoneyAmount(){
-        return this.moneyAmount;
+    public int getGoldAmount(){
+        return this.goldAmount;
     }
 
-    public void addMoneyAmount(int moneyChange){
-        this.moneyAmount += moneyChange;
+    public void setGoldAmount(int goldAmount){
+        this.goldAmount = goldAmount;
         // TODO Must be changed to add the amount of money the animal costs when sold
 
     }
 
-    public ArrayList<Animal>getAnimals(){
+    public void addGold(int goldAdded){
+        this.goldAmount += goldAdded;
+    }
+
+    public void removeGold(int goldRemoved){
+        this.goldAmount -= goldRemoved;
+    }
+
+    public ArrayList<Animal> getAnimalList(){
         return this.animals;
     }
 
