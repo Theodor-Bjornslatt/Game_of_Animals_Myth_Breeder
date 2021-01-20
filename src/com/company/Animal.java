76ca64a80714e58a2cf8161e70caaf52c387e726
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-01-20 11:07:16 +0100

    Made loseHealth() work
    Fixed formatting
    
    Deleted todo-items

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -19,6 +19,7 @@ public abstract class Animal {
     private String name;
     private String gender;
     private int health = 100;
+    private int healthChange = 0;
     private int maxOffspring;
 
     public Animal(String name, String gender){
@@ -49,17 +50,17 @@ public abstract class Animal {
 
     public String getSpecies(){
         return this.getClass().getSimpleName();
-    }//TODO check if working
+    }
 
     public void setOnlyEat(Food food){
         onlyEat.add(food);
     }
 
     public String getOnlyEat(){
-        String onlyEats =this.getClass().getSimpleName() + "s only eats ";
+        String onlyEats =this.getClass().getSimpleName() + "s only eat: ";
         // For each food in list onlyEat, get string value of class;
         for(Food food : onlyEat){
-            String next = food.getClass().toString();
+            String next = "\n" + food.getClass().toString();
             onlyEats += next;
         }
         return onlyEats;
@@ -73,20 +74,24 @@ public abstract class Animal {
 
     }
 
-    public int loseHealth(){
-        //TODO set lostHealth to random number between 10 and 30
-        int lostHealth = 10;
+    public void loseHealth(){
+        // Make the lost amount of health be a random number from 10 to 30
+        Random randomNum = new Random();
+        healthChange = randomNum.nextInt(21) + 10;
         // If lostHealth is bigger than health, return
         // remaining health as lostHealth and let health reach zero
         // Else, subtract lostHealth from health
-        if(lostHealth >= health){
-            lostHealth = health;
-            this.health = 0;
+        if(healthChange >= health){
+            healthChange = health;
+            health = 0;
         }
         else{
-            this.health -= lostHealth;
+            health -= healthChange;
         }
-        return lostHealth;
+    }
+
+    public int getLostHealth(){
+        return healthChange;
     }
 
 
