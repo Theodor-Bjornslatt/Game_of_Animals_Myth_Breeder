459f95f6a6879e62d7ab2eabc38c71dea3ad5b41
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-01-20 11:07:55 +0100

    Changed todo-item

--- src/com/company/Food.java
+++ src/com/company/Food.java
@@ -2,9 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 
-// TODO Add integer to keep track of amounts of the same food
-//  Make method to remove one item from food by reducing
-//  double keeping track of food
+// TODO
 //  If double reaches 0, remove foodItem from list completely
 
 //  TODO Make method to add one item to food by adding one to the integer
