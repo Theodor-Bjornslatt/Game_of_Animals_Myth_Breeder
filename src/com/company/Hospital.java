2d83e4974381078157404766f833fad1771035b3
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-02-03 16:58:37 +0100

    Fixed issue where an animal that died was removed from the list before it's name was printed.

--- src/com/company/Hospital.java
+++ src/com/company/Hospital.java
@@ -9,7 +9,7 @@ public class Hospital {
 
     public static void goToHospital(){
         treatedAnimals = false;
-        HelperMethods.clearConsole();
+        Helper.clearConsole();
         System.out.println("\n" + doctor + ":\n" +
                 "Welcome, " + Game.getCurrentPlayer().getName() + ", to my hospital for mythological animals. ");
 
@@ -20,20 +20,20 @@ public class Hospital {
                 return;
             }
 
-            String answer = HelperMethods.yesOrNo(
+            String answer = Helper.yesOrNo(
                     "\nDo you have a diseased animal that needs treatment? (y/n)");
 
             if(answer.equals("y")){
-                HelperMethods.printPlayerAnimals();
+                Helper.printPlayerAnimals();
                 System.out.println("\n" + doctor + ":\nEach treatment costs " + treatmentCost + " Gold. " +
                         "\nWhich animal do you want me to try to cure?");
-                HelperMethods.chooseAnimal();
+                Helper.chooseAnimal();
                 tryTreatment();
             }
             else{
-                HelperMethods.clearConsole();
+                Helper.clearConsole();
                 System.out.println("\n" + doctor + ":\nGoodbye! May good health be with you and yours!");
-                HelperMethods.setValidChoice(treatedAnimals);
+                Helper.setValidChoice(treatedAnimals);
                 return;
             }
         }
@@ -41,32 +41,33 @@ public class Hospital {
 
     public static void tryTreatment(){
         if(Game.getCurrentPlayer().getGoldAmount() < treatmentCost ){
-            HelperMethods.clearConsole();
+            Helper.clearConsole();
             System.out.println(doctor + ":\nOh dear! It appears you don't have enough " +
                     "gold to pay for a treatment.\nI'm afraid you'll have to leave for now.");
-            HelperMethods.setValidChoice(treatedAnimals);
+            Helper.setValidChoice(treatedAnimals);
             noTreatmentAvailable = true;
         }
-        else if(HelperMethods.chosenAnimal.getHealthStatus()==HealthStatus.DISEASED){
-            boolean successful = HelperMethods.fiftyPerChance();
-            Animal treatedAnimal = Game.getCurrentPlayer().getAnimalList().get(HelperMethods.getInputInt()-1);
+        else if(Helper.chosenAnimal.getHealthStatus()==HealthStatus.DISEASED){
+            boolean successful = Helper.fiftyPerChance();
+            Animal treatedAnimal = Game.getCurrentPlayer().getAnimalList().get(Helper.getInputInt()-1);
             if(successful){
                 Game.getCurrentPlayer().removeGold(treatmentCost);
                 treatedAnimal.setHealthStatus(HealthStatus.HEALTHY);
                 treatedAnimal.gainHealth(100);
                 treatedAnimals = true;
                 System.out.println("\n" + doctor + ":\nI am happy to tell you the treatment worked! " +
-                        HelperMethods.chosenAnimal.getName() + " is now completely healthy and well fed.");
+                        Helper.chosenAnimal.getName() + " is now completely healthy and well fed.");
 
             }
             else{
-                System.out.println(doctor + ":\nI am so sorry, but " + HelperMethods.chosenAnimal.getName() +
+                System.out.println(doctor + ":\nI am so sorry, but " + Helper.chosenAnimal.getName() +
                         " did not respond to my treatment and has sadly passed away.");
+                Game.getCurrentPlayer().getAnimalList().remove(Helper.chosenAnimal);
                 treatedAnimals = true;
             }
         }
         else{
-            System.out.println("\n" + doctor + ":\n" + HelperMethods.chosenAnimal.getName() +
+            System.out.println("\n" + doctor + ":\n" + Helper.chosenAnimal.getName() +
                     " appears to be perfectly healthy! I think my time is better spent elsewhere.");
         }
     }
