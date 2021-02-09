b7855b8afbf99bb4ad63ee69b50594ead4f34a18
Theodor-Bjornslatt <theodor.b.1995@gmail.com>
2021-02-09 19:11:21 +0100

    Made it so Nykurs don't eat helpless humans

--- src/com/company/animals/Nykur.java
+++ src/com/company/animals/Nykur.java
@@ -2,18 +2,15 @@ package com.company.animals;
 
 import com.company.enums.Gender;
 import com.company.enums.Species;
-import com.company.foods.HelplessHuman;
 import com.company.foods.Seaweed;
 
 public class Nykur extends Animal {
-    HelplessHuman helplessHuman = new HelplessHuman();
     Seaweed seaweed = new Seaweed();
 
     public Nykur(String name, Gender gender){
         super(name, gender, Species.NYKUR);
         this.setMaxOffspring(3);
         this.setHungerSatisfaction(10);
-        addOnlyEat(helplessHuman);
         addOnlyEat(seaweed);
     }
 
